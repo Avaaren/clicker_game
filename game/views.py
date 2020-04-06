40c9ba3722d488e6e610dc0e27aa639ab1e2b110
Avaaren <avaaren@mail.ru>
2020-04-06 13:25:04 +0300

    Added main page view

--- game/views.py
+++ game/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class GameView(TemplateView):
+
+    template_name = 'game/game.html'
+
+        
