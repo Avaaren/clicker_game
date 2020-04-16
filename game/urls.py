482df1ba6adfd79dbfbee9ba861af19ab931809d
Avaaren <avaaren@mail.ru>
2020-04-16 13:58:58 +0300

    Added urls for ajax and result functions

--- game/urls.py
+++ game/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.GameView.as_view(), name='game'),
+    path('ajax_result/', views.ajax_result, name='ajax_result'),
+    path('result/', views.ResultView.as_view(), name='result'),
 ]
