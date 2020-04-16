4f3f9a024cba5e3dbab0a08ee23348d0704ac9ea
Avaaren <avaaren@mail.ru>
2020-04-16 13:59:49 +0300

    All models registered on admin site

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    GameSession,
+    Profile
+)
+
+
+@admin.register(GameSession)
+class GameSessionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'score', 'time')
+
+
+@admin.register(Profile)
+class ProfileSessionAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ('pk', 'user', 'games_played', 'high_score')
