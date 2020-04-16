7620c6b6da5913b45727f3a83f03b7f11b417c51
Avaaren <avaaren@mail.ru>
2020-04-16 13:57:03 +0300

    Added CBV returning result template with user result and scoreboard

--- game/views.py
+++ game/views.py
@@ -37,7 +37,28 @@ def ajax_result(request):
         return JsonResponse(data)
 
 
+class ResultView(TemplateView):
 
+    template_name = 'game/result.html'
+
+    def get_context_data(self, **kwargs):
+        game_obj = GameSession.objects.filter(user=self.request.user).order_by('-time')[:1][0]
+        high_scores = GameSession.objects.all().order_by('-score')[:3]
+        lower_results = GameSession.objects.filter(score__lte=game_obj.score).exclude(pk=game_obj.pk).order_by('-score', '-time')[:1]
+        higher_results = GameSession.objects.filter(score__gte=game_obj.score).exclude(pk__in=[game_obj.pk, lower_results[0].pk]).order_by('score', 'time')[:1]
+        for index, el in enumerate(GameSession.objects.order_by('-score', '-time')):
+            if game_obj.pk == el.pk:
+                your_place = index + 1
+
+        data = super().get_context_data(**kwargs)
+        if game_obj in high_scores:
+            data['is_in_highscores'] = True
+        data['game_result'] = game_obj
+        data['high_scores'] = high_scores
+        data['your_place'] = your_place
+        data['lower_results'] = lower_results
+        data['higher_results'] = higher_results
+        return data
 
 
 
