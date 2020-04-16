64976e547cafe1f6ae8f68e847d1e1633a1922c6
Avaaren <avaaren@mail.ru>
2020-04-16 17:33:14 +0300

    Added authentication urls

--- game/urls.py
+++ game/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,4 +7,36 @@ urlpatterns = [
     path('', views.GameView.as_view(), name='game'),
     path('ajax_result/', views.ajax_result, name='ajax_result'),
     path('result/', views.ResultView.as_view(), name='result'),
+
+    # Auth urls
+    path('login/',
+         auth_views.LoginView.as_view(
+             template_name='auth/login.html'),
+         name='login'),
+
+    path('logout/',
+         auth_views.LogoutView.as_view(),
+         name='logout'),
+
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='auth/password_reset.html',
+             email_template_name='auth/password_reset_email.html'),
+         name='password_reset'),
+
+    path('password_reset_done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='auth/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('password_reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='auth/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('password_reset_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+            template_name='auth/password_reset_complete.html'),
+         name='password_reset_complete'),
+    
 ]
