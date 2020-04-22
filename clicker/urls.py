68d6009125f730d8b0ea4cdd0efed0b9cc664a16
Avaaren <avaaren@mail.ru>
2020-04-22 12:52:22 +0300

    Included auth urls in proj urls

--- clicker/urls.py
+++ clicker/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('game.urls')),
+    path('account/', include('account.urls', namespace='account')),
 ]
