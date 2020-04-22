9644693c076bb58e97cdcc12a6fe711fe4a334cd
Avaaren <avaaren@mail.ru>
2020-04-22 12:56:57 +0300

    Registered profile model in account admin file

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from game.models import (
+    Profile,
+)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    class Meta:
+        list_display = ('pk', 'user', 'games_played', 'high_score')
